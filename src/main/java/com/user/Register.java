ce9533825cbd2e5846015991d0760b82025d07ab
pritesh-peter <pritesh.peter@gmail.com>
2022-08-12 16:53:02 +0545

    WIP connecting with mysql db

---
+++ src/main/java/com/user/Register.java
@@ -0,0 +1,124 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.user;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Pritesh Singh
+ */
+public class Register extends HttpServlet {
+
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Register</title>");            
+            out.println("</head>");
+            out.println("<body>");
+//getting all the incoming detail from  reguest
+        
+            String name = request.getParameter("user_name");
+            String email = request.getParameter("user_email");
+            String password = request.getParameter("user_password");
+            
+            //connection..............
+            try{
+                Class.forName("com.mysql.jdbc.Driver");
+                
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/trn","root","Password1");
+            
+                //query.............
+                String q="insert into user(name,password,email) values(?,?,?)";
+                
+                PreparedStatement pstmt = con.prepareStatement(q);
+                pstmt.setString(1,name);
+                pstmt.setString(2,password);
+                pstmt.setString(3, email);
+                
+                pstmt.executeUpdate();
+                
+                out.println("<h1>Registered Sccessfully</h1>");
+                
+            }catch (ClassNotFoundException e) {
+                out.println(e.getMessage());
+            }
+
+
+
+          
+          //................
+            
+
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
